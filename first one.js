807b1b20ada5a2d29438419a2f83592cfef16ffa
sankar <140276408+sankarfinn@users.noreply.github.com>
2023-09-02 04:19:56 +0530

    Add files via upload

---
+++ first one.js
@@ -0,0 +1,18 @@
+// // console.log("guvi class");
+// function addTwoNum(a, b)
+// return a + b
+
+// let num = 10
+// let num1 = 20
+// // nodelet sum =
+// console.log function addTwoNum(num, num1)
+
+let num = {
+  name: " sankar",
+  place: "chennai",
+  work: "guvi",
+  age: "25",
+};
+num.name = "finn";
+delete num.name;
+console.log(num);
